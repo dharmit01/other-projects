8429d21e9ff14aa123b276b1a40c1f20b7e1bb73
Dharmit Shah <81405914+dharmit01@users.noreply.github.com>
2021-06-18 11:51:09 +0530

    Add files via upload

---
+++ random_password_generator/index.js
@@ -0,0 +1,33 @@
+// ABCDEFGHIJKLMNOPQRSTUVWXWZ
+// abcdefghijklmnopqrstuvwxwz
+// !\"$%&/()=?@~'\\.\';:+=^*_-"
+// 0123456789
+
+const characters = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxwz!\"$%&/()=?@~'\\.\';:+=^*_-0123456789";
+let output = document.getElementById("output")
+
+function randomValue(value){
+    return Math.floor(Math.random()*value);
+}
+
+function genPassword(){
+    let length = document.getElementById("length").value;
+    document.getElementById("length-val").textContent = length;
+    let str = '';
+
+    for(let i=0; i<length; i++){
+        let random = randomValue(characters.length);
+        str += characters.charAt(random);
+    }
+    output.value = str;
+}
+
+function copyClipboard(){
+    output.select();
+    document.execCommand('copy');
+    alert("Password Copied to Clipboard!");
+}
+genPassword();
+
+
